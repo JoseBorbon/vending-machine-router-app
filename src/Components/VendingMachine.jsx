b8fbbf8ecca347c90cae50ba14b429aa99d1a151
JoseBorbon <joserborbon@gmail.com>
2021-11-01 18:09:27 -0400

    add routing to VendingMachine component

--- src/Components/VendingMachine.jsx
+++ src/Components/VendingMachine.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './VendingMachine.css';
 
 class VendingMachine extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="VendingMachine-container">
+        <div className="VendingMachine-welcome">
+          <h1>HELLO I AM A VENDING MACHINE WHAT WOULD YOU LIKE TO HAVE?</h1>
+        </div>
+        <nav className="VendingMachine-nav">
+          <Link exact to="/soda">
+            SODA
+          </Link>
+          <Link exact to="/chips">
+            CHIPS
+          </Link>
+          <Link exact to="/sardines">
+            SARDINES
+          </Link>
+        </nav>
+      </div>
+    );
   }
 }
 
