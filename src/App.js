16c2b3a650cea80908be7260c073b164740f6814
JoseBorbon <joserborbon@gmail.com>
2021-11-01 07:33:59 -0400

    base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { NavLink, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Switch>
+          <NavLink exact path="/" render={() => <VendingMachine />} />
+          <NavLink exact path="/soda" render={() => <Soda />} />
+          <NavLink exact path="/chips" render={() => <Chips />} />
+          <NavLink exact path="/sardines" render={() => <Sardines />} />
+        </Switch>
+      </nav>
     </div>
   );
 }
