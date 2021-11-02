640c12cc714ecc9d72da3657149d3ae0c4ac29a0
JoseBorbon <joserborbon@gmail.com>
2021-11-02 14:22:50 -0400

    applied basename for deployment

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/vending-machine-router-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
