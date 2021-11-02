78ca35c2c9da12b915a4e3416234753d84771caf
JoseBorbon <joserborbon@gmail.com>
2021-11-01 21:59:13 -0400

    rename className to be more specific to component

--- src/Components/Soda.jsx
+++ src/Components/Soda.jsx
@@ -5,19 +5,19 @@ import './Soda.css';
 class Soda extends Component {
   render() {
     return (
-      <div className="VendingMachine-soda-container">
+      <div className="Soda-container">
         <img
-          className="VendingMachine-soda-can-one"
+          className="Soda-can-one"
           src="https://purepng.com/public/uploads/large/purepng.com-coca-cola-cancoca-colacokecarbonated-soft-drinksoft-drinkcoke-can-1411527233399fihip.png"
         />
-        <div className="VendingMachine-back-container">
-          <Link className="VendingMachine-go-back" exact to="/">
+        <div className="Soda-back-container">
+          <Link className="Soda-go-back" exact to="/">
             Go Back
           </Link>
           <h2>GULP GULP GULP</h2>
         </div>
         <img
-          className="VendingMachine-soda-can-two"
+          className="Soda-can-two"
           src="https://purepng.com/public/uploads/large/purepng.com-coca-cola-cancoca-colacokecarbonated-soft-drinksoft-drinkcoke-can-1411527233399fihip.png"
         />
       </div>
