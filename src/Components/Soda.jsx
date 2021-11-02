547473d5a44c59f0f55ae615092c28afd3f7ed75
JoseBorbon <joserborbon@gmail.com>
2021-11-01 21:27:42 -0400

    add soda routing

--- src/Components/Soda.jsx
+++ src/Components/Soda.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Soda.css';
 
 class Soda extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="VendingMachine-soda-container">
+        <img
+          className="VendingMachine-soda-can-one"
+          src="https://purepng.com/public/uploads/large/purepng.com-coca-cola-cancoca-colacokecarbonated-soft-drinksoft-drinkcoke-can-1411527233399fihip.png"
+        />
+        <div className="VendingMachine-back-container">
+          <Link className="VendingMachine-go-back" exact to="/">
+            Go Back
+          </Link>
+          <h2>GULP GULP GULP</h2>
+        </div>
+        <img
+          className="VendingMachine-soda-can-two"
+          src="https://purepng.com/public/uploads/large/purepng.com-coca-cola-cancoca-colacokecarbonated-soft-drinksoft-drinkcoke-can-1411527233399fihip.png"
+        />
+      </div>
+    );
   }
 }
 
