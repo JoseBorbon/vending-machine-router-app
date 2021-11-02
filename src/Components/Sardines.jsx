3188de04121b28a416d03cd6f3c6fd074a435d6a
JoseBorbon <joserborbon@gmail.com>
2021-11-01 21:57:23 -0400

    add sardines routing

--- src/Components/Sardines.jsx
+++ src/Components/Sardines.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Sardines.css';
 
 class Sardines extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="Sardines-container">
+        <div className="Sardines-back-container">
+          <Link className="Sardines-go-back" exact to="/">
+            Go Back
+          </Link>
+          <h2>HA! YOU DIDNT EXPECT THIS!</h2>
+        </div>
+      </div>
+    );
   }
 }
 
